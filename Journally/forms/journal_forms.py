e3db458da5666bb744c7039f476241922b68776a
peejaykoy12345 <rick.well.rick@gmail.com>
2025-08-13 20:45:22 +0800

    Created journal forms

---
+++ Journally/forms/journal_forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class CreateJournalForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    create = SubmitField("Create Journal")
+
+class CreateJournalPageForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()])
+    create = SubmitField("Create Page")
+
+class EditJournalPageForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()])
+    create = SubmitField("Save Edits")
