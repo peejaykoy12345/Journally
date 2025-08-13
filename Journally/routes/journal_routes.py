960405b170e981888762a2dfe79eabc2c5997375
peejaykoy12345 <rick.well.rick@gmail.com>
2025-08-13 20:54:19 +0800

    Added create journal.html and list.html

--- Journally/routes/journal_routes.py
+++ Journally/routes/journal_routes.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from Journally.models import Journal, JournalPage
 from Journally.forms.journal_forms import CreateJournalForm, CreateJournalPageForm, EditJournalPageForm
 
 journal_bp = Blueprint("journal", __name__)
 
-@journal_bp.route("/")
+@journal_bp.route("/journals")
+@login_required
+def journals():
+    journals = Journal.query.filter_by(owner_id=current_user.id).all()
+
+    return render_template("journal/list.html", journals=journals)
+
 @journal_bp.route("/create_journal")
 def create_journal():
-    return render_template("journal/create_journal.html")
+    form = CreateJournalForm()
+    return render_template("journal/create_journal.html", form=form)
